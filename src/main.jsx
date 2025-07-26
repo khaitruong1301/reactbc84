e192e0685675f153744cbdff0c979ad7a8562177
Adobe Pham <xuanhau031297@gmail.com>
2025-07-26 17:03:06 +0700

    buoi4_state_props_luyentap_tiep_theo

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,7 @@
-import { createRoot } from 'react-dom/client'
-import BT1_HomeComponent from './components/BT1_HomeComponent'
+import { createRoot } from 'react-dom/client';
 //import css
 import "./css/index.css";
-import DataBinding from './databinding/DataBinding';
-import HandleEvent from './HandleEvent/HandleEvent';
-import RenderCondition from './render_condition/RenderCondition';
-import DemoState from './RenderStatement/DemoState';
-import DemoChangeNumber from './RenderStatement/DemoChangeNumber';
-import DemoChangeColor from './RenderStatement/DemoChangeColor';
-import DemoTinker from './RenderStatement/DemoTinker';
-import DemoStyle from './StyleComponent/DemoStyle';
-import DemoProps from './Props/DemoProps';
-import BT_ProductList from './Props/BTProps/BT_ProductList';
 import BT_StateProps from './Props/BT_StateProps/BT_StateProps';
-import BT_ChonXe from './Props/BT_ChonXe/BT_ChonXe';
 
 
 
