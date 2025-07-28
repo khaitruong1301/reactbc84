1800f533a1cb502da8e3d5648c570e88be517270
khai <khaitruong1301@gmail.com>
2025-07-28 14:06:40 +0700

    buoi4_5_btgiohang_api

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,14 @@ import { createRoot } from 'react-dom/client';
 //import css
 import "./css/index.css";
 import BT_StateProps from './Props/BT_StateProps/BT_StateProps';
+import ProductsPage from './api/ProductsPage';
+import ToDoListApi from './api/ToDoListApi';
 
-
+/*
+  Phân biệt state và props:
+  state: giá trị thay đổi dùng để binding lên giao diện, state có thể gán lại giá trị ( state,setState)
+  props: giá trị có thể thay đổi dùng để binding lên giao diện, props không thể gán lại giá trị (readonly)
+*/
 
 createRoot(document.getElementById('root')).render(
   <>
@@ -18,11 +24,11 @@ createRoot(document.getElementById('root')).render(
     {/* <DemoStyle /> */}
     {/* <DemoProps /> */}
     {/* <BT_ProductList /> */}
-    <BT_StateProps />
+    {/* <BT_StateProps /> */}
     {/* <BT_ChonXe /> */}
-    
+    {/* <ProductsPage /> */}
+    <ToDoListApi />
   </>
 )
-
 
 
