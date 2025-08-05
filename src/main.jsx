04df8992980edca20d19efc7f204ba1d13373109
khai <khaitruong1301@gmail.com>
2025-08-05 14:30:48 +0700

    buoi7_reactrouterdom

--- src/main.jsx
+++ src/main.jsx
@@ -6,32 +6,52 @@ import ProductsPage from './api/ProductsPage';
 import ToDoListApi from './api/ToDoListApi';
 import DemoFormLogin from './Form/DemoFormLogin';
 import DemoFormik from './Form/DemoFormik';
-
-/*
-  Phân biệt state và props:
-  state: giá trị thay đổi dùng để binding lên giao diện, state có thể gán lại giá trị ( state,setState)
-  props: giá trị có thể thay đổi dùng để binding lên giao diện, props không thể gán lại giá trị (readonly)
-*/
-
+import { BrowserRouter, Outlet, Route, Routes } from 'react-router-dom'
+import HomeIndex from './Pages/HomeIndex';
+import Login from './Pages/Login'
+import HomeTemplate from './Templates/HomeTemplate';
+import Register from './Pages/Register';
+import DemoUseNavigate from './Pages/ReactRouterDom/DemoUseNavigate';
+import ForGotPass from './Pages/ReactRouterDom/ForGotPass';
+import DemoUseParam from './Pages/ReactRouterDom/DemoUseParam';
+import HeaderHome from './Templates/HeaderHome';
+import DemoUseSearchParam from './Pages/ReactRouterDom/DemoUseSearchParam';
 createRoot(document.getElementById('root')).render(
   <>
-   {/* <BT1_HomeComponent /> */}
-   {/* <DataBinding /> */}
-   {/* <HandleEvent /> */}
-    {/* <RenderCondition /> */}
-    {/* <DemoState /> */}
-    {/* <DemoChangeNumber /> */}
-    {/* <DemoChangeColor /> */}
-    {/* <DemoTinker /> */}
-    {/* <DemoStyle /> */}
-    {/* <DemoProps /> */}
-    {/* <BT_ProductList /> */}
-    {/* <BT_StateProps /> */}
-    {/* <BT_ChonXe /> */}
-    {/* <ProductsPage /> */}
-    {/* <ToDoListApi /> */}
-    {/* <DemoFormLogin /> */}
-    <DemoFormik />
+    <BrowserRouter>
+      <Routes>
+        <Route path='' element={<HomeTemplate />}>
+          <Route index element={<HomeIndex />}></Route>
+          <Route path='login' element={<Login />}></Route>
+          <Route path='register' element={<Register />}></Route>
+        </Route>
+
+        <Route path='api' element={<div>
+          <header className='bg-dark text-white p-3'>Header</header>
+          <Outlet />
+          <footer className='bg-dark text-white p-3'>footer</footer>
+        </div>}>
+          <Route path='todolist' element={<ToDoListApi />} />
+          <Route path='productpage' element={<ProductsPage />} />
+        </Route>
+
+        <Route path='react-router-dom' element={<HomeTemplate />}>
+          <Route path='demo-use-navigate' element={<DemoUseNavigate />}></Route>
+          <Route path='for-got-pass' element={<ForGotPass />}></Route>
+          <Route path='use-search-param' element={<DemoUseSearchParam />}></Route>
+          
+        </Route>
+        <Route path='demo-use-param'>
+          <Route path=':id' element={
+            <div>
+              <HeaderHome />
+              <DemoUseParam />
+            </div>
+          }></Route>
+        </Route>
+
+      </Routes>
+    </BrowserRouter>
   </>
 )
 
